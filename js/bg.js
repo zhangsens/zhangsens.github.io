fbba698c49c0498c96c571f82200ba97a7ad3e80
zhangss <zssqqzss@vip.qq.com>
2017-09-13 23:40:55 +0800

    bg

--- js/bg.js
+++ js/bg.js
@@ -1,7 +1,6 @@
-const body = document.querySelector(".body");
-const bg = document.createElement("div");
-bg.className = "bg";
-const ul = document.createElement("ul");
+const head = document.head;
+const style = document.createElement("style");
+head.append(style);
 const list = [`48433698_p0.jpg`,
     `64899108_p0.png`,
     `64876862_p0_master1200.jpg`,
@@ -14,7 +13,7 @@ const list = [`48433698_p0.jpg`,
     `59888608_p0.jpg`,
     `60396265_p0.jpg`
 ];
-const li = [];
+const _style = [];
 
 for (var i = 0; i < 5; i++) {
     var int = Math.floor(Math.random() * (list.length + 1));
@@ -22,9 +21,30 @@ for (var i = 0; i < 5; i++) {
         i--;
         continue;
     }
-    li.push(`<li style="background:url(../img/bg/${list[int]}) no-repeat center;"></li>`);
+    _style.push(`.bg ul li:nth-child(${i+1}){
+        background:url(../img/bg/${list[int]}) no-repeat center fixed;
+        background-size:cover;
+    }`);
 }
+style.innerHTML = _style.join("");
 
-body.append(bg);
-bg.append(ul);
-ul.innerHTML = li.join("");
+var nth = 0;
+const lis = document.querySelectorAll(".bg li");
+
+function bgAnimate() {
+    nth = nth > lis.length - 1 ? nth++ : 0;
+    for (var i = 0; i < lis.lenght; i++) {
+        if (i == nth) {
+            document.querySelector(".bg li").style.transform = `scale(1.2)`;
+            document.querySelector(".bg li").style.opacity = 1;
+        } else {
+            document.querySelector(".bg li").style.transform = `scale(1)`;
+            document.querySelector(".bg li").style.opacity = 1;
+        }
+
+    }
+    setTimeout(bgAnimate, 5000);
+}
+//bgAnimate();
+//看效果用
+document.querySelector(".bg li").style.opacity = 1;
